42705ec5374cbe3c79b6919c87f6a00548d3001c
hj0128 <guswn2332@gmail.com>
2024-01-18 13:34:44 +0900

    feat: Create Splash

---
+++ app/src/main/java/com/example/in_time/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.in_time;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
